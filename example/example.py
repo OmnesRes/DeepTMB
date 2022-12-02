57e0de36f59afb191f55c6d9d7e4a138438b29bf
Jordan <omnesresnetwork@gmail.com>
2022-12-02 17:16:00 -0500

    ipython notebook

--- example/example.py
+++ example/example.py
@@ -1,5 +1,8 @@
+import matplotlib
+matplotlib.use('TKAgg')
 import pathlib
 import sys
+from matplotlib import pyplot as plt
 path = pathlib.Path.cwd()
 if path.stem == 'DeepTMB':
     cwd = path
@@ -31,7 +34,7 @@ data = pd.read_csv(cwd / 'example' / 'example_training_data.txt', sep=',')
 X = data['panel_values'].values
 Y = data['exome_values'].values
 
-##no weighting in this example
+##no weighting in this example, if you care about a certain TMB region can add weights
 y_weights = np.ones_like(Y)
 
 ##log transform and make loaders
@@ -40,7 +43,6 @@ X = t.trf(X[:, np.newaxis])
 Y = t.trf(Y)
 X_loader = utils.Map.PassThrough(X)
 Y_loader = utils.Map.PassThrough(Y)
-Y_loader_fcn = utils.Map.PassThrough(Y[:, np.newaxis])
 W_loader = utils.Map.PassThrough(y_weights)
 
 ##build graph
@@ -70,26 +72,68 @@ net.model.fit(ds_train,
               callbacks=callbacks
               )
 
-##define where to calculate probabilities
+##define where to calculate probabilities (should cover range of possible Y distribution)
 y_pred = np.linspace(0, np.max(Y + .5), 1000)
 
 ##check the benefit of the fit
 
-##get median predictions
+##get predictions for a range of values(median)
+x_pred = np.linspace(np.min(X), np.max(X), 200)
+pred_quantiles = [y_pred[np.argmin(np.diff((np.exp(net.model(x_pred).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.05).astype(int), axis=0), axis=0)],
+             y_pred[np.argmin(np.diff((np.exp(net.model(x_pred).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.25).astype(int), axis=0), axis=0)],
+             y_pred[np.argmin(np.diff((np.exp(net.model(x_pred).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.5).astype(int), axis=0), axis=0)],
+             y_pred[np.argmin(np.diff((np.exp(net.model(x_pred).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.75).astype(int), axis=0), axis=0)],
+            y_pred[np.argmin(np.diff((np.exp(net.model(x_pred).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.95).astype(int), axis=0), axis=0)]]
+
+##make a plot to check fit
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=.98,
+bottom=0.108,
+left=0.1,
+right=.987,
+hspace=0.2,
+wspace=0.2)
+ax.scatter(X, Y, s=5, edgecolor='none', alpha=.3)
+for i in range(len(pred_quantiles)):
+    if i == 0:
+        ax.plot(x_pred, pred_quantiles[i], color='k', linestyle='dashed', alpha=.5, label='Percentiles (5, 25, ' + r'$\bf{50}$' + ', 75, 95)')
+    elif i == 2:
+        ax.plot(x_pred, pred_quantiles[i], color='k', linewidth=2, linestyle='dashed', alpha=1)
+    else:
+        ax.plot(x_pred, pred_quantiles[i], color='k', linestyle='dashed', alpha=.5)
+ax.set_xticks([t.trf(i) for i in [0, 2, 5, 10, 25, 50]])
+ax.set_xticklabels([0, 2, 5, 10, 25, 50])
+ax.set_yticks([t.trf(i) for i in [0, 2, 5, 10, 20, 35, 60]])
+ax.set_yticklabels([0, 2, 5, 10, 20, 35, 60], fontsize=12)
+ax.set_xlabel('Panel-Derived TMB', fontsize=16)
+ax.set_ylabel('Exomic TMB', fontsize=16)
+ax.tick_params(length=3, width=1)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_position(['outward', 5])
+ax.spines['left'].set_bounds(t.trf(0), t.trf(60))
+ax.spines['bottom'].set_bounds(t.trf(0), t.trf(50))
+plt.legend(frameon=False, loc=(.01, .95), fontsize=10)
+ax.set_ylim(t.trf(0), t.trf(60))
+ax.set_xlim(-.01, t.trf(50))
+
+
+##get predictions for all the data (median)
 medians = y_pred[np.argmin(np.diff((np.exp(net.model(X).log_cdf(y_pred[:, np.newaxis]).numpy()) < 0.5).astype(int), axis=0), axis=0)]
 
-##mean absolute error in original data space prior to fit
+##mean absolute error for panel TMB (the input to the model)
 print(np.mean(np.abs(t.inv(Y) - t.inv(X[:, 0]))))
 
-##mean absolute error in original data space
+##mean absolute error for predictions
 print(np.mean(np.abs(t.inv(Y) - t.inv(medians))))
 
 ##for only panel TMB >= 5
 mask = t.inv(X[:, 0]) >= 5
-##mean absolute error in original data space prior to fit
+##mean absolute error for panel TMB (the input to the model)
 print(np.mean(np.abs(t.inv(Y[mask]) - t.inv(X[:, 0][mask]))))
 
-##mean absolute error in original data space
+##mean absolute error for predictions
 print(np.mean(np.abs(t.inv(Y[mask]) - t.inv(medians[mask]))))
 
 ##load data to predict on
@@ -108,8 +152,3 @@ pd.DataFrame(data={'5th percentile': t.inv(quantiles[0]),
                    '50th percentile': t.inv(quantiles[2]),
                    '75th percentile': t.inv(quantiles[3]),
                    '95th percentile': t.inv(quantiles[4])}).to_csv(cwd / 'example' / 'example_predictions.txt', index=False, sep='\t')
-
-
-
-
-
